64cd4e9e642e0c8ce5e5d2e20eab6493caea42d8
JakeR-wq <jacobr10183@gmail.com>
2024-02-23 13:59:56 -0500

    working on datawriting/loading

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -1,5 +1,23 @@
 package src;
 
+import java.io.FileReader;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;;
+
 public class DataLoader extends DataConstants {
+    public static ArrayList<User> loadUsers() {
+        ArrayList<User> user = new ArrayList<User>();
+        try {
+            FileReader reader = new FileReader(USER_FILE_NAME);
+            JSONParser parser = new JSONParser();
+            JSONArray userJSON = (JSONArray) new JSONParser().parse(reader);
+            return user;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
 
 }

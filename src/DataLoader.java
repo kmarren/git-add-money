f01b38a21426ab51db95a1746a746a692a2cfb71
jakeR-wq <jacobr10183@gmail.com>
2024-02-27 12:25:31 -0500

    working on #40, removing studentID #39

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -4,36 +4,119 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;;
+import org.json.simple.parser.JSONParser;
 
 public class DataLoader extends DataConstants {
 
-    public static ArrayList<User> loadUsers() {
-        ArrayList<User> users = new ArrayList<>();
-        try {
-            FileReader reader = new FileReader(USER_FILE_NAME);
-            JSONParser parser = new JSONParser();
-            JSONArray userJSON = (JSONArray) parser.parse(reader);
+    public static ArrayList<Student> loadStudents() {
+        ArrayList<Student> students = new ArrayList<>();
+    try {
+        FileReader reader = new FileReader(STUDENT_FILE_NAME);
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(reader);
 
-            for (Object obj : userJSON) {
-                JSONObject userObj = (JSONObject) obj;
-                int type = Integer.parseInt(userObj.get(USER_TYPE).toString());
-                String firstName = (String) userObj.get(USER_FIRST_NAME);
-                String lastName = (String) userObj.get(USER_LAST_NAME);
-                String email = (String) userObj.get(USER_EMAIL);
-                String username = (String) userObj.get(USER_USERNAME);
-                String password = (String) userObj.get(USER_PASSWORD);
-                if (type == 1) {
-                    users.add(new Student(firstName, lastName, email, username, password));
-                } else {
-
+        if (obj instanceof JSONArray) {
+            JSONArray studentJSON = (JSONArray) obj;
+            // Process array of students
+            for (Object studentObj : studentJSON) {
+                JSONObject studentJsonObj = (JSONObject) studentObj;
+                // Parse student data and create Student objects
+                String firstName = (String) studentJsonObj.get(STUDENT_FIRST_NAME);
+                String lastName = (String) studentJsonObj.get(STUDENT_LAST_NAME);
+                String email = (String) studentJsonObj.get(STUDENT_EMAIL);
+                String username = (String) studentJsonObj.get(STUDENT_USERNAME);
+                String password = (String) studentJsonObj.get(STUDENT_PASSWORD);
+                Major major = new Major();
+                boolean minor = (boolean) studentJsonObj.get(STUDENT_MINOR);
+                JSONArray commentsArray = (JSONArray) studentJsonObj.get(STUDENT_COMMENTS);
+                ArrayList<String> studentComments = new ArrayList<>();
+                for (Object commentObj : commentsArray) {
+                    String comment = (String) commentObj;
+                    studentComments.add(comment);
                 }
-
+                JSONArray achievementsArray = (JSONArray) studentJsonObj.get(STUDENT_ACHIEVEMENTS);
+                ArrayList<Achievement> achievements = new ArrayList<>();
+                /* 
+                for (Object achievementObj : achievementsArray) {
+                    JSONObject achievementJson = (JSONObject) achievementObj;
+                    String achievementID = (String) achievementJson.get("achievementID");
+                    achievements.add(achievementID);
+                }
+                */
+                double gpa = (double) studentJsonObj.get("gpa");
+                JSONArray enrolledCoursesArray = (JSONArray) studentJsonObj.get("enrolledCourses");
+                ArrayList<Course> enrolledCourses = new ArrayList<>();
+                /* 
+                for (Object courseObj : enrolledCoursesArray) {
+                    JSONObject courseJson = (JSONObject) courseObj;
+                    String courseID = (String) courseJson.get("courseID");
+                    enrolledCourses.add(courseID);
+                }
+                */
+                Advisor advisor = null; // add an advisor later??
+                boolean riskFailing = (boolean) studentJsonObj.get("riskFailing");
+                double hoursCompleted = (double) studentJsonObj.get("hoursCompleted");
+                
+                // Create Student object
+                Student student = new Student(gpa, minor, studentComments, enrolledCourses, achievements, advisor, riskFailing,
+                hoursCompleted, firstName, lastName, email, username, password, major);
+                
+                // Add student to the list
+                students.add(student);
             }
-            return users;
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else if (obj instanceof JSONObject) {
+            JSONObject studentJsonObj = (JSONObject) obj;
+            // Process single student object
+            // Parse student data and create Student object
+            String firstName = (String) studentJsonObj.get(STUDENT_FIRST_NAME);
+            String lastName = (String) studentJsonObj.get(STUDENT_LAST_NAME);
+            String email = (String) studentJsonObj.get(STUDENT_EMAIL);
+            String username = (String) studentJsonObj.get(STUDENT_USERNAME);
+            String password = (String) studentJsonObj.get(STUDENT_PASSWORD);
+            Major major = new Major();
+            boolean minor = (boolean) studentJsonObj.get(STUDENT_MINOR);
+            JSONArray commentsArray = (JSONArray) studentJsonObj.get(STUDENT_COMMENTS);
+            ArrayList<String> studentComments = new ArrayList<>();
+            for (Object commentObj : commentsArray) {
+                String comment = (String) commentObj;
+                studentComments.add(comment);
+            }
+            JSONArray achievementsArray = (JSONArray) studentJsonObj.get(STUDENT_ACHIEVEMENTS);
+            ArrayList<Achievement> achievements = new ArrayList<>();
+            /* 
+            for (Object achievementObj : achievementsArray) {
+                JSONObject achievementJson = (JSONObject) achievementObj;
+                String achievementID = (String) achievementJson.get("achievementID");
+                achievements.add(achievementID);
+            }
+            */
+            double gpa = (double) studentJsonObj.get("gpa");
+            JSONArray enrolledCoursesArray = (JSONArray) studentJsonObj.get("enrolledCourses");
+            ArrayList<Course> enrolledCourses = new ArrayList<>();
+            /* 
+            for (Object courseObj : enrolledCoursesArray) {
+                JSONObject courseJson = (JSONObject) courseObj;
+                String courseID = (String) courseJson.get("courseID");
+                enrolledCourses.add(courseID);
+            }
+            */
+            Advisor advisor = new Advisor();
+            boolean riskFailing = (boolean) studentJsonObj.get("riskFailing");
+            double hoursCompleted = (double) studentJsonObj.get("hoursCompleted");
+            
+            // Create Student object
+            Student student = new Student(gpa, minor, studentComments, enrolledCourses, achievements, advisor, riskFailing,
+            hoursCompleted, firstName, lastName, email, username, password, major);
+            
+            // Add student to the list
+            students.add(student);
+        } else {
+            // Handle unsupported JSON format
+            System.err.println("Unsupported JSON format");
         }
-        return null;
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return students;
     }
 }

4c9eee433de0c14a96397d71a603dee402cdc82b
JakeR-wq <jacobr10183@gmail.com>
2024-03-11 16:47:01 -0400

    datawriting #41

--- src/temporaryUI.java
+++ src/temporaryUI.java
@@ -1,14 +1,10 @@
 package src;
 
-public class temporaryUI 
-{
-
+public class temporaryUI {
 
     private static Application application;
 
-
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
         application = Application.getInstance();
         System.out.println(application.login("alicejohnson456", "securepass456"));
         application.signUp("timmyballer", "swagwrod", 1);
@@ -16,6 +12,6 @@ public class temporaryUI
         System.out.println(application.login("timmyballer", "swagwrod"));
         System.out.println(application.login("johndoe", "balsdj"));
         System.out.println(application.getCourseList().toString());
-
+        DataWriter.saveUsers();
     }
 }
